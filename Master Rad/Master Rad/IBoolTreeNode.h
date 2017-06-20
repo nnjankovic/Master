eb4cfe00c46f1d7b983c03992516912497496bf5
nnjankovic <shone23jankovic@gmail.com>
2017-06-20 15:40:34 +0200

    Interfaces added

--- Master Rad/Master Rad/IBoolTreeNode.h
+++ Master Rad/Master Rad/IBoolTreeNode.h
@@ -10,22 +10,34 @@
 #include <memory>
 #include <string>
 
+#define INTF struct
+
+/*! This enum value is 
+	used to determine wheter node is operator or operand
+*/
 enum NodeType {
 	OPERATOR,
 	OPERAND
 };
 
-class IBoolTreeNode
+/*! This class provides an interface  
+	that represents a node in BoolTree data structure.
+*/
+INTF IBoolTreeNode
 {
 public:
+	/*! Returns the type of a node
+	*/
 	virtual NodeType getNodeType() = 0;
+
+	/*! Returns a smart pointer to left or right child of the node if it exists 
+		or nullpointer otherwise.
+	*/
 	virtual std::shared_ptr<IBoolTreeNode> getLeftChildNode() = 0;
 	virtual std::shared_ptr<IBoolTreeNode> getRightChildNode() = 0;
+
+	/*! Returns the string representation of node.
+	*/
 	virtual std::string toString() = 0;
-
-	virtual ~IBoolTreeNode(){}
 };
-
-
-
 #endif
--- Master Rad/Master Rad/OperatorNode.h
+++ Master Rad/Master Rad/OperatorNode.h
@@ -1,15 +1,67 @@
+/**
+*	@file OperatorNode.h
+*	@Author Nenad Jankovic 3204//15
+*
+*	This file contains declaration of OperatorNode interface.
+*/
+
 #pragma once
 #ifndef OPERATORNODE_H_
 #define OPERATORNODE_H_
 
 #include "IBoolTreeNode.h"
 
+/*! This enum value determines wheter operator is AND(conjunction) or
+	OR(disjunction) operator.
+*/
+enum OperatorType {
+	AND, //*
+	OR	//+
+};
+
+/*! This abstract class implements IBoolTreeNode and
+	represents operator node in BoolTree data structure.
+*/
 class OperatorNode :
 	public IBoolTreeNode
 {
 public:
-	OperatorNode();
-	virtual ~OperatorNode();
+	/*! Constructor initializes both child nodes to nullptr and isNegated flag to false. 
+	*/
+	OperatorNode() : leftChild(nullptr),
+					 rightChild(nullptr),
+					 isUnderNegation(false)
+	{}
+
+	// IBoolTreeNode implementations
+	NodeType getNodeType() { return OPERATOR; }
+	std::shared_ptr<IBoolTreeNode> getLeftChildNode() { return leftChild; }
+	std::shared_ptr<IBoolTreeNode> getRightChildNode() { return rightChild; }
+	virtual std::string toString() = 0;
+
+	/*! Sets a left child node.
+	*/
+	void setLeftChildNode(std::shared_ptr<IBoolTreeNode> node) { leftChild = node; }
+	/*! Sets a right child node.
+	*/
+	void setRightChildNode(std::shared_ptr<IBoolTreeNode> node) { rightChild = node; }
+
+	/*! This function is called to determine wheter this operator in under negation.
+	*/
+	bool isOperationNegation() { return isUnderNegation; }
+	/*! Sets if operator is under negation.
+	*/
+	void setIsUnderNegation(bool value) { isUnderNegation = value;}
+
+	/*! Virtual destructor.
+	*/
+	virtual ~OperatorNode() {}
+private:
+	std::shared_ptr<IBoolTreeNode> leftChild;
+	std::shared_ptr<IBoolTreeNode> rightChild;
+
+	// This value is an indication wheter this operation is under negation.
+	bool isUnderNegation;
 };
 
 #endif // !OPERATORNODE_H_
